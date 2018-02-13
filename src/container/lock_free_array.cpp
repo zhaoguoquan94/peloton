213a66fdb558546cbe90adad09677b7564fe5fff
vkonagar <vkonagar@andrew.cmu.edu>
2018-02-13 00:49:37 -0500

    Add attachThread() to the API. Need to move this later to the callers.

--- src/container/lock_free_array.cpp
+++ src/container/lock_free_array.cpp
@@ -32,8 +32,8 @@ class IndirectionArray;
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 LOCK_FREE_ARRAY_TYPE::LockFreeArray(){
   // Initialize hazard pointer GC.
-  cds::gc::hp::GarbageCollector::Construct(3, 100, 1);
-  LOG_DEBUG("Initalized lock free array");
+  cds::gc::hp::GarbageCollector::Construct(10, 100, 10);
+  LOG_DEBUG("Initialized lock free array");
 }
 
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
@@ -45,6 +45,7 @@ LOCK_FREE_ARRAY_TYPE::~LockFreeArray(){
 
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 bool LOCK_FREE_ARRAY_TYPE::Update(const std::size_t &offset, ValueType value){
+  cds::threading::Manager::attachThread();
   // Insert into offset (key) with value.
   LOG_DEBUG("Update at %lu", offset);
   return lock_free_list.upsert(offset, value, false).first;
@@ -52,6 +53,7 @@ bool LOCK_FREE_ARRAY_TYPE::Update(const std::size_t &offset, ValueType value){
 
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 bool LOCK_FREE_ARRAY_TYPE::Append(ValueType value){
+  cds::threading::Manager::attachThread();
   LOG_DEBUG("Appended at %lu", lock_free_array_offset.load());
   return lock_free_list.insert(lock_free_array_offset++, value);
 }
@@ -59,6 +61,7 @@ bool LOCK_FREE_ARRAY_TYPE::Append(ValueType value){
 // invalid_value: not used.
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 bool LOCK_FREE_ARRAY_TYPE::Erase(const std::size_t &offset){
+  cds::threading::Manager::attachThread();
   LOG_DEBUG("Erase at %lu", offset);
   return lock_free_list.erase(offset);
 }
@@ -66,6 +69,7 @@ bool LOCK_FREE_ARRAY_TYPE::Erase(const std::size_t &offset){
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 ValueType LOCK_FREE_ARRAY_TYPE::Find(const std::size_t &offset, const ValueType& invalid_value) const{
   LOG_DEBUG("Find at %lu", offset);
+  cds::threading::Manager::attachThread();
   auto it = lock_free_list.find(offset);
   if (it == lock_free_list.end()) {
     return invalid_value;
@@ -76,6 +80,7 @@ ValueType LOCK_FREE_ARRAY_TYPE::Find(const std::size_t &offset, const ValueType&
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 ValueType LOCK_FREE_ARRAY_TYPE::FindValid(const std::size_t &offset,
                                           const ValueType& invalid_value) const {
+  cds::threading::Manager::attachThread();
   LOG_DEBUG("Find Valid at %lu", offset);
   std::size_t off = 0;
   auto it = lock_free_list.begin();
@@ -90,6 +95,7 @@ ValueType LOCK_FREE_ARRAY_TYPE::FindValid(const std::size_t &offset,
 
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 size_t LOCK_FREE_ARRAY_TYPE::GetSize() const{
+  cds::threading::Manager::attachThread();
   auto size = lock_free_list.size();
   LOG_DEBUG("Size: %lu", size);
   return size;
@@ -97,6 +103,7 @@ size_t LOCK_FREE_ARRAY_TYPE::GetSize() const{
 
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 bool LOCK_FREE_ARRAY_TYPE::IsEmpty() const{
+  cds::threading::Manager::attachThread();
   auto isEmpty = lock_free_list.empty();
   LOG_DEBUG("IsEmpty: %d", isEmpty);
   return isEmpty;
@@ -104,11 +111,13 @@ bool LOCK_FREE_ARRAY_TYPE::IsEmpty() const{
 
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 void LOCK_FREE_ARRAY_TYPE::Clear() {
+  cds::threading::Manager::attachThread();
   lock_free_list.clear();
 }
 
 LOCK_FREE_ARRAY_TEMPLATE_ARGUMENTS
 bool LOCK_FREE_ARRAY_TYPE::Contains(const ValueType& value) {
+  cds::threading::Manager::attachThread();
   bool exists = false;
   auto it = lock_free_list.begin();
   while(it != lock_free_list.end()) {
